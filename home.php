39e9e1ba2e31aab943e904aae414ec38664837b1
Arturo Ochoa <mendozaarturo1221@gmail.com>
2021-11-19 16:42:24 -0600

    header listo pagina home lista junto con paginas de articulos

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <html lang="es">
 
 <head>
-    <title>Vibranium Blog</title>
+    <title>Rome Blog</title>
     <link rel="icon" type="image/png" href="../../img/icono.png" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,132 +15,59 @@
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <!-- Brand -->
-            <a class="navbar-brand" href="#"><img src="../../img/logoBlogW.png" alt="logo" width="170px" height="60px"></a>
+    <!--HEADER-->
+    <?php include("./templates/header.php")?>
 
-            <!-- Toggler/collapsibe Button -->
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <!-- Navbar links -->
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                </ul>
-
-                <form class="form-inline my-2 my-lg-0">
-                    <li class="nav-item dropdown" style="list-style-type:none;">
-                        <label class="my-2 my-sm-0" style="font-size: 24px; margin-right: 60px;">
-                            <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">                                
-                                <i class="fa fa-user" style="margin-left: 10px;"></i>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">                               
-
-                                <a class="dropdown-item" href="#" hidden>Another action</a>
-                                <a class="dropdown-item" href="#" hidden>Something else here</a>
-                            </div>
-                        </label>
-                    </li>
-                </form>
-            </div>
-        </nav>
-        <hr style="background-color:white; margin-bottom: 2px;">
-        <nav class="navbar navbar-expand-lg navbar-dark" style="padding-top:0px;">
-
-            <!-- Navbar links -->
-            <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="articulos.php">Artículos</a>
-                    </li>                   
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <div id="contenedorPrincipal">
-        <div class="text-center">
-            <h1 aling="center"><b><i>B I E N V E N I D O</i></b></h1>
-            <hr class="border-primary">
-
-        </div>
-        <br>
-        <div id="intro">
-            <h3><b>¿Qué es la física?</b></h3>
-            <p id="descripcion">Es una ciencia fundamental que estudia y describe el
-                comportamiento
-                de los fenómenos naturales que
-                ocurren en nuestro universo. Es una ciencia basada en observaciones experimentales y en mediciones. Su
-                objetivo es desarrollar teorías físicas basadas en leyes fundamentales, que permitan describir el mayor
-                número posible de fenómenos naturales con el menor número posible de leyes físicas. Estas leyes físicas se
-                expresan en lenguaje matemático, por lo que para entender sin inconvenientes el tratamiento del formalismo
-                teórico de los fenómenos físicos se debe tener una apropiada formación en mate-máticas, en este curso basta
-                un nivel básico de matemáticas.</p>
-        </div>
-        <br>
-        <div id="carru" class="carousel slide pad-5" data-ride="carousel">
-            <ul class="carousel-indicators">
-                <li data-target="#demo" data-slide-to="0" class="active"></li>
-                <li data-target="#demo" data-slide-to="1"></li>
-                <li data-target="#demo" data-slide-to="2"></li>
-                <li data-target="#demo" data-slide-to="3"></li>
-            </ul>
-
-            <div class="carousel-inner border border-info rounded pt-3 pl-3 pr-3">               
-            </div>
-
-            <!-- Left and right controls -->
-            <a class="carousel-control-prev" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-            </a>
-            <a class="carousel-control-next" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </a>
-        </div>
-        <br><br>
-
-        <div id="intro" style="padding-left: 3%; padding-right: 3%;">
-            <div class="row ">
-                <div class="col-md-6 border-right border-info text-center">
-                    <h3><b>Décadas más populares</b></h3>
-                    <a href="vistas/articulos.php?decada=9" class="btn btn-outline-primary btn-lg w-75 mb-3 mt-3" type="submit">Década de los 90's </a>
-                    <br>
-                    <a href="vistas/articulos.php?decada=8" class="btn btn-outline-primary btn-lg w-75 mb-3" type="submit">Década de los 80's </a>
-                    <br>
-                    <a href="vistas/articulos.php?decada=7" class="btn btn-outline-primary btn-lg w-75 mb-3" type="submit">Década de los 70's </a>
-                    <br>
-                    <a href="vistas/articulos.php?decada=6" class="btn btn-outline-primary btn-lg w-75 mb-3" type="submit">Década de los 60's </a>
-                </div>
-                <div class="col-md-6 text-center">
-                    <h3><b>Artículos por Siglo</b></h3>
-                    <a href="vistas/articulos.php?siglo=20" class="btn btn-outline-primary btn-lg w-75 mb-3 mt-3" type="submit">
-                        Siglo XX
-                    </a>
-                    <br>
-                    <a href="vistas/articulos.php?siglo=21" class="btn btn-outline-primary btn-lg w-75 mb-3 mt-3" type="submit">
-                        Siglo XXI
-                    </a>
-                </div>
-            </div>
-        </div>
-        <br>
-
-        <div id="intro" class="mb-5">
-            <div class="text-center mt-3 mb-4">
-                <h2><b>Artículos Destacados</b></h2>
-            </div>
-            <div class="row mt-3">
-                <div class="col-md-6 text-justify pad-5">                   
-                </div>
-                <div class="col-md-6 text-justify pad-5">                    
-                </div>
-            </div>
-        </div>
+    <div class="text-center">
+            <h1 aling="center"><b><i>BIENVENIDO</i></b></h1>
+            <h2 aling="center"><b><i>Blog acerca de edificios historicos en la ciudad de Roma</i></b></h2>
+            <hr class="my-4">
     </div>
+
+    <div id="contenedorPrincipal">    
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="img/coliseo.jpg" height="728px" width="410px"  alt="First slide">     
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Coliseo romano</h5>
+                        <p>"El mayor anfiteatro romano"</p>
+                    </div>               
+                </div>
+                
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/foro.jpg" height="686px" width="386px" alt="Second slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Foro Romano</h5>
+                        <p>"El corazon de la antigua Roma"</p>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="img/palatino.jpg" alt="Third slide">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Monte Palatino</h5>
+                        <p>"El hogar de los emperadores en Roma"</p>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+        <br>
+        
+        
+        
+    </div>
+
     <!--FOOTER-->
     <?php include("./templates/footer.php")?>
 
